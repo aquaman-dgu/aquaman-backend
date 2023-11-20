be5a5f737bfe26568f429c18b3794d340f928d13
gsandoo <rogersddt@naver.com>
2023-11-21 01:42:34 +0900

    Chore: initial setting

---
+++ src/test/java/com/example/aquamanbackend/AquamanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aquamanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquamanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
